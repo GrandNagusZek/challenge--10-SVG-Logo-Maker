3521f462b09115207089cb78e0ccecc1e27450fd
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-03-10 21:08:15 -0400

    completed test for circle

--- test/circle.test.js
+++ test/circle.test.js
@@ -1,8 +1,10 @@
 const Circle=require("../lib/circle")
+const circle= new Circle("IM", "blue", "black")
+
 describe("Circle", ()=>{
     describe("properties", ()=>{
         it("test text, textColor, and shapeColor",()=>{
-            const circle= new Circle("IM", "blue", "black")
+            
             //input is circle.text = output IM
             expect(circle.text).toEqual("IM")
             expect(circle.textColor).toEqual("blue")
@@ -12,7 +14,8 @@ describe("Circle", ()=>{
 
     describe("method (is function in an object)", ()=> {
         it("test render ()", ()=> {
-
+            //input render() = '`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><circle cx="150" cy="100" r="80" fill="${this.shapeColor}"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`'
+            expect(circle.render()).toEqual(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><circle cx="150" cy="100" r="80" fill="black"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="blue">IM</text></svg>`)
         })
     })
 })
