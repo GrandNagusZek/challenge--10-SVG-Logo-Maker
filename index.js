ff03fc8f9ce3f7744d6b42aec8d6ec67687390d9
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-03-12 00:49:46 -0400

    completed code for inquirer prompt to generate circle and square shapes and to write svg files

--- index.js
+++ index.js
@@ -31,15 +31,25 @@ const questions =[
 inquirer.prompt(questions)
 .then(data=>{
     if(data.shape==="circle"){
-
+        const circle = new Circle(data.text, data.textColor, data.shapeColor)
+        fs.writeFile("./examples/circle.svg", circle.render(), err=> {
+            if(err)console.error(error)
+            console.log("Success!")
+        })
     }
     else if(data.shape==="triangle"){
         const triangle = new Triangle(data.text, data.textColor, data.shapeColor)
-        fs.writeFile("./examples/circle.svg", triangle.render(), err=> {
+        fs.writeFile("./examples/triangle.svg", triangle.render(), err=> {
             if(err)console.error(err)
-            console.log("success")
+            console.log("Success!")
         })
     }else{
         (data.shape==="square")
+            const square = new Square(data.text, data.textColor, data.shapeColor)
+            fs.writeFile("./examples/square.svg", square.render(), err=>{
+                if(err)console.error(error)
+                console.log("Success!")
+            })
+        
     }
 })
